e86754da0c5db22f215b4f45fbe27c791596c4b2
legop3 <minecratfd8@gmail.com>
2025-09-27 23:24:19 -0400

    update text some

--- MatrixIndicator/src/main.cpp
+++ MatrixIndicator/src/main.cpp
@@ -41,14 +41,17 @@ void setup() {
   // delay(1000);
 
   // show fullscreen rainbow on start
-  matrix.drawLine(5, 0, W-1, 0, matrix.Color(255,   0,   0)); // red
-  matrix.drawLine(5, 1, W-1, 1, matrix.Color(255, 127,   0)); // orange
-  matrix.drawLine(5, 2, W-1, 2, matrix.Color(255, 255,   0)); // yellow
-  matrix.drawLine(5, 3, W-1, 3, matrix.Color(0,   255,   0)); // green
-  matrix.drawLine(5, 4, W-1, 4, matrix.Color(0,     0, 255)); // blue
-  matrix.drawLine(5, 5, W-1, 5, matrix.Color(75,    0, 130)); // indigo
-  matrix.drawLine(5, 6, W-1, 6, matrix.Color(148,    0, 211)); // violet
+  matrix.drawLine(0, 0, W-1, 0, matrix.Color(255,   0,   0)); // red
+  matrix.drawLine(0, 1, W-1, 1, matrix.Color(255, 127,   0)); // orange
+  matrix.drawLine(0, 2, W-1, 2, matrix.Color(255, 255,   0)); // yellow
+  matrix.drawLine(0, 3, W-1, 3, matrix.Color(0,   255,   0)); // green
+  matrix.drawLine(0, 4, W-1, 4, matrix.Color(0,     0, 255)); // blue
+  matrix.drawLine(0, 5, W-1, 5, matrix.Color(75,    0, 130)); // indigo
+  matrix.drawLine(0, 6, W-1, 6, matrix.Color(148,    0, 211)); // violet
   
+  matrix.show();
+  delay(1000);
+
   // show more important colors on left side :3
   matrix.drawLine(0, 0, 0, 7, matrix.Color(0,   255, 255)); // light blue
   matrix.drawLine(1, 0, 1, 7, matrix.Color(255, 105, 180)); // pink
@@ -60,11 +63,23 @@ void setup() {
   delay(3000);
 
   matrix.fillScreen(0);
+
+  // permanent rainbow across top row, 2 pixel wide lines
+  // matrix.drawLine(0, 0, 2, 0, matrix.Color(255,   0,   0)); // red
+  // matrix.drawLine(3, 0, 4, 0, matrix.Color(255, 127,   0)); // orange
+  // matrix.drawLine(5, 0, 6, 0, matrix.Color(255, 255,   0)); // yellow
+  // matrix.drawLine(7, 0, 8, 0, matrix.Color(0,   255,   0)); // green
+  // matrix.drawLine(9, 0, 10, 0, matrix.Color(0,     0, 255)); // blue
+  // matrix.drawLine(11, 0, 12, 0, matrix.Color(75,    0, 130)); // indigo
+  // matrix.drawLine(13, 0, 15, 0, matrix.Color(148,    0, 211)); // violet
+
+
   matrix.show();
 }
 
 void showEv(bool m = false) {
-  matrix.fillScreen(0);
+  // matrix.fillScreen(0);
+  matrix.fillRect(0, 1, W, H-1, 0); // clear everything except top rainbow line
 
   matrix.drawXBitmap(0, 0, EV_bits, EV_width, EV_height, matrix.Color(0, 255, 0));
 
@@ -76,7 +91,8 @@ void showEv(bool m = false) {
 }
 
 void showGas() {
-  matrix.fillScreen(0);
+  // matrix.fillScreen(0);
+  matrix.fillRect(0, 1, W, H-1, 0); // clear everything except top rainbow line
 
   matrix.drawXBitmap(0, 0, GAS_bits, GAS_width, GAS_height, matrix.Color(255, 162, 0));
 
